c27520daf81b2eca04578e789533f4f3a14300e2
Maxime Schwartz <Max@mylocalhost.local>
2013-12-08 20:48:37 +0100

    Minor

--- code/site/kernel/HTTPResponse.class.php
+++ code/site/kernel/HTTPResponse.class.php
@@ -1,21 +1,11 @@
 <?php
 namespace Kernel;
 
-<<<<<<< HEAD
-	protected $page;
-
-class HTTPResponse{
-=======
 class HTTPResponse {
 
 	// TODO
 	public function send() {
 		//send the response of the server
-	}
->>>>>>> 09c470e306cd25977485055bbd50005ddf3498c4
-
-	public send() {
-		exit($page->get);
 	}
 
 }
